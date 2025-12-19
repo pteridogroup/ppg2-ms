faa7c4e05b287a4be18662abfeedd1496b7d7135
joelnitta <joelnitta@gmail.com>
2025-12-19 14:46:48 +0900

    Add conflict management

--- R/packages.R
+++ R/packages.R
@@ -1,3 +1,10 @@
+# Load conflicted first to manage function conflicts
+library(conflicted)
+
+# Set conflict preferences
+conflicted::conflicts_prefer(tidyr::replace_na)
+
+# Load other packages
 library(tidyverse)
 library(targets)
 library(tarchetypes)
